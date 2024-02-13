463fe8da5cedfecf77166309a80a0728d296d6c8
Stehno Jan <st72710@upce.cz>
2024-02-13 17:50:38 +0100

    Initial commit

---
+++ src/main/java/cz/upce/fei/nnpia/cviceni/CviceniApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpia.cviceni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CviceniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CviceniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/nnpia/cviceni/CviceniApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.nnpia.cviceni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CviceniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
